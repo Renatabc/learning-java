3b91bdfa1c287b6cf84caab5b56588ca7b73782f
Renata Brito Costa <93830634+Renatabc@users.noreply.github.com>
2022-10-16 14:06:53 +0000

    Exemplos de tipos e variáveis

--- Renata/edu.Renata.segundaParte/TiposeVariaveis.java
+++ Renata/edu.Renata.segundaParte/TiposeVariaveis.java
@@ -1,3 +1,12 @@
 public class TiposeVariaveis{
-  
+  public static void main(final String[] args){
+    final byte idade = 13;
+    final short ano = 2021;
+    final int cep = 59865000;
+    final long cpf = 23452111133L; 
+    final float pi = 3.14F;
+    final double salario = 1212.14;
+
+    //os tipos float e long precisam terminar com F e L, respectivamente, a fim de que o sistema não os confundam com outros tipos.
+  }
 }
