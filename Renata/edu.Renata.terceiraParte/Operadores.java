a45b1727e312c5bdd0cd9d4c649925fc746ccabf
Renata Brito Costa <93830634+Renatabc@users.noreply.github.com>
2022-10-18 19:15:42 +0000

    Breve estudo de Operadores Lógicos

---
+++ Renata/edu.Renata.terceiraParte/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores{
+  //Atribuição "="
+  //Exemplos:
+  //String nome = "Renata";
+  //int idade = 27;
+  //double peso = 60.2;
+  //char sexo = 'F';
+  //boolean doador = true;
+  //Date dataNascimento = new Date();
+
+  //Aritméticos "+, -, *, %, /"
+  
+  //Unários: junto com outro operador aritmético, podem incrementar, decrementar, inverter numéricos e booleanos. "+, -, ++, --, !"
+
+  //Ternário: forma resumida de definir uma condição entre dois valores. "Condição ? resultado1 : resultado2" 
+
+  //Relacionais: avaliam a relação entre duas variáveis ou expressões. "==, !, <, >, <=, >="
+
+  //Lógicos: e (&&) ou (||)
+}
